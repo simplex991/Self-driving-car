b20a9e8ad41fad49a7b70038429981610914e3dc
simplex991 <stefanh.arnason@outlook.com>
2019-03-23 22:44:15 +0100

    specified the region of interest

--- Finding-Lanes/lanes.py
+++ Finding-Lanes/lanes.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
 
 def canny(image):
     gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
@@ -7,9 +8,19 @@ def canny(image):
     canny = cv2.Canny(blur, 50, 150)
     return canny
 
+def region_of_interest(image):
+    height = image.shape[0]
+    polygons = np.array([
+    [(200, height), (1100, height), (550, 250)]
+    ])
+    mask = np.zeros_like(image)
+    cv2.fillPoly(mask, polygons, 255)
+    masked_image = cv2.bitwise_and(image, mask)
+    return masked_image
 
 image = cv2.imread('test_image.jpg')
 lane_image = np.copy(image)
 canny = canny(lane_image)
-cv2.imshow('result', canny)
+cropped_image = region_of_interest(canny)
+cv2.imshow('result', region_of_interest(cropped_image))
 cv2.waitKey(0)
